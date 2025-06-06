e3cd1174c7ea1aad75f34203cf391540964272c1
han3o1 <skqieum@gmail.com>
2025-06-06 20:09:54 +0900

    fix typo

--- challedger-backend/routes/expenses.js
+++ challedger-backend/routes/expenses.js
@@ -4,8 +4,8 @@ const expenseController = require('../controllers/expenseController');
 const auth = require('../middleware/auth');  // 🔒 JWT authentication middleware
 
 router.post('/', auth, expenseController.createExpense); // Register a new expense
-router.get('all/', auth, expenseController.getExpenses); // Get all expenses
-router.get('/', auth, expenseController.getExpenses); // Get all expenses for a given month
+router.get('all/', auth, expenseController.getAllExpenses); // Get all expenses
+router.get('/', auth, expenseController.getMonthlyExpenses); // Get all expenses for a given month
 router.delete('/:id', auth, expenseController.deleteExpense); // Delete expense by ID
 
 // Export the router
